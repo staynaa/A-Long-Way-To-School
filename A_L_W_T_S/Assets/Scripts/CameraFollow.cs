3e35eaf95886064bd643470a89b88ae083c18b46
CeeDee <charles_dubreuil053100@yahoo.com>
2023-03-27 03:11:51 -0400

    Implemented Camera Bound Feature To CameraFollow Script

--- A_L_W_T_S/Assets/Scripts/CameraFollow.cs
+++ A_L_W_T_S/Assets/Scripts/CameraFollow.cs
@@ -4,32 +4,55 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
+  [Tooltip("Reference to object to be followed by camera")]
   // Reference to object to be followed
   public Transform target;
 
+  [Tooltip("Camera offset: Keep Z at -10 ")]
   // Camera Offset
   public Vector3 offset;
 
+  [Tooltip("Camera Follow Smoothness Range [1:(slower) - 10:(faster)] ")]
   //Camera Follow Smoothness
   [Range(1,10)]
   public float smoothFactor;
+
+  [Tooltip("Min/Max bound values for camera")]
+  // Bound values for camera 
+  public Vector3 minValues, maxValues;
 
 
 
 
     
-    // FixedUpdate is called once every time step is settled
-    void FixedUpdate() 
-    {
-      Follow();
-    }
+  // FixedUpdate is called once every time step is settled
+  void FixedUpdate() 
+  {
+    Follow();
+  }
 
-    //Update the camera coordinates based on the object being followed coordinates 
-    void Follow( )
-    {  
-      Vector3 targetPosition = target.position + offset;
-      Vector3 smoothPosition = Vector3.Lerp(transform.position,targetPosition,smoothFactor * Time.fixedDeltaTime);
-      transform.position = smoothPosition;
-   }
+
+
+  /* 
+  Method Name: Follow()
+  Description: Update the camera coordinates based on the object being followed coordinates 
+  */    
+    
+  void Follow( )
+  {  
+  Vector3 targetPosition = target.position + offset;
+  //Verify if the targetPosition is out of bounds or not
+  //Limit it to the min an max values
+
+  //Bounded position of camera 
+  Vector3 boundPosition = new Vector3(
+    Mathf.Clamp(targetPosition.x,minValues.x,maxValues.x),
+    Mathf.Clamp(targetPosition.y,minValues.x,maxValues.y),
+    Mathf.Clamp(targetPosition.z,minValues.x,maxValues.z));
+
+
+  Vector3 smoothPosition = Vector3.Lerp(transform.position,boundPosition,smoothFactor * Time.fixedDeltaTime);
+  transform.position = smoothPosition;
+  }
    
 }
