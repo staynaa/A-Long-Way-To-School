fda40ae3b07cf22be946d1b71ade72e9ff1ef0ad
Stayna <staynaa.cs@gmail.com>
2023-04-15 12:47:23 -0400

    copied files over to main
    nyc level folder and the meta file
    USA-level scene and it's meta
    scripts-> objMove.cs, parallaxMove.cs, restartUSA.cs, showWarning.cs, spawner.cs, busArrival, player movement
    
    THIS LEVEL USES CINEMACHINE CAMERA

--- A_L_W_T_S/Assets/Scripts/PlayerMovement.cs
+++ A_L_W_T_S/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     //Initialize Animator object 
     private Animator animator;
     
-     #endregion
+    #endregion
 
 
     #region Collider Reference Variables
@@ -70,39 +70,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Tooltip("Vertical power of player")]
     // Vertical Movement Power Of Player
-    [SerializeField] private float verticalPower = 6;
-
-    [Header("Stamina Variables")]
-
-    
-    [Tooltip("Maximum Stamina")]
-    //Maximum stamina value
-    [SerializeField] private float maxStamina = 100;
-
-    [Tooltip("Current Stamina")]
-    //Player current stamina
-    [SerializeField] private float currentStamina;
-
-
-    [Tooltip("Stamina Use Multiplier")]
-    //Value at which player stamina depletes at when in use  
-    [SerializeField] private float staminaUseMultiplier = 5;
-
-    [Tooltip("Stamina Regen Start Timer")]
-    //Time before stamina regeneration starts
-    [SerializeField] private float timeBeforeStaminaRegenStarts = 5;
-
-
-    [Tooltip("Stamina Regen Value")]
-    //Value at which player player stamina regenerate at when not in use
-    [SerializeField] private float staminaValueIncrement = 2;
-
-
-    [Tooltip("Stamina Regen per frame delay timer")]
-    //Time delay between each frame at which stamina regenerate at
-    [SerializeField] private float staminaTimeIncrement = 0.1f;
-    
-    [SerializeField] private Coroutine regeneratingStamina;
+    [SerializeField] private float verticalPower = 75;
     
     #endregion
 
@@ -126,20 +94,14 @@ public class PlayerMovement : MonoBehaviour
     //Determine If Player Is Running
     private bool isRunning = false;
 
+    //Determine If Player Is Jumping;
+    private bool jump = false;
+
     //Determine If Player Is Grounded 
     private bool isGrounded = false;
 
-    //Determine If Player Is Coyote Jumping
-    bool coyoteJump;
-
     //Determine if Player is crouched button is pressed/held
     private bool crouchedPressed = false;
-
-    //Determine if player is using stamina 
-    private bool useStamina = true;
-
-    //Determine if player is able to run
-    private bool canRun = true;
 
     #endregion
 
@@ -152,45 +114,29 @@ public class PlayerMovement : MonoBehaviour
 
         //Reference to player's Animator
         animator = GetComponent<Animator>();
-
-        //Initialize player starting stamina
-        currentStamina = maxStamina;
     }
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
-
     // Update is called once per frame
      void Update()
     {
-        //Locks players movement 
-        if (CanMove() == false)
-        {
-            return;
-        }
-
         //Set the yVelocity in the animator 
         animator.SetFloat("yVelocity",rb.velocity.y);
  
         // Store value given by the key presses of button associated horinzontal movement
         horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-
-        if(useStamina)
-        {
-            HandleStamina();
-        }
-
         
         // If left-shift is clicked and held down enable isRunning 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && canRun)
+        if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            isRunning = true; 
+            isRunning = true;
         }
 
         // If left-shift is released disable isRunning 
@@ -199,11 +145,20 @@ public class PlayerMovement : MonoBehaviour
             isRunning = false;
         }
 
+
         //If jump button is pressed/helded jump is enable
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            jump  = true;
+            animator.SetBool("Jump",true);
         }
+
+        //If jump button is released jump is disable
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jump = false;
+        }
+
 
         //If Crouch button is pressed/helded Crouch is enable
         if (Input.GetButtonDown("Crouch"))
@@ -223,41 +178,20 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate() 
     {
         GroundCheck();
-        move(horizontalMovement,crouchedPressed);
-    }
-    
-    /* 
-    Method Name: CanMove() 
-    Description: Checks if player should be able to move or not.
-    */ 
-    bool CanMove()
-    {
-        bool can = true;
-        if(FindObjectOfType<InteractionSystem>().isExamining)
-        {
-            can = false;
-        }
-
-        if(FindObjectOfType<InventorySystem>().isOpen)
-        {
-            can = false;
-        }
-        
-        return can;
+        move(horizontalMovement,jump,crouchedPressed);
     }
 
 
-    /* 
+
+/* 
     Method Name: groundCheck() 
     Description: Check If The GroundCheckObject Is Colliding With Other, 
     2D Collider That Are In The "Ground" Layer If Yes (isGrounded true) Else (isGrounded false)
-    */  
+*/  
     
     void GroundCheck()
 
-     {  
-        // Track previous state of isGrounded Variable 
-        bool wasGrounded = isGrounded;
+     {
         isGrounded = false;
 
 
@@ -266,20 +200,6 @@ public class PlayerMovement : MonoBehaviour
         if(colliders.Length > 0)
         {
             isGrounded = true;
-
-            //Detect landing of play *Double jump placeholder*
-            if(!wasGrounded)
-            {
-
-            }
-        }
-        
-        else
-        {
-            if(wasGrounded)
-            {
-                StartCoroutine(CoyoteJumpDelay()); 
-            }
         }
 
         //As long as we are grounded the "Jump" Bool;
@@ -287,53 +207,21 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("Jump",!isGrounded);
      } 
 
-    
-    
-    // Timer for coyote jump to be enable
-     IEnumerator CoyoteJumpDelay()
-     {
-        coyoteJump = true;
-        yield return new WaitForSeconds(0.2f);
-        coyoteJump = false;
-     }
-
-    
-    /* 
-    Method Name: Jump()
-    Description: Determine if player is grounded if player is 
-    grounded allow them to jump if player is not grounded disbales jumping
-    */
-    void Jump()
-    {
-        //If Crouch Is Pressed Disable The Standing Collider + Enable animate crouching + Reduce Speed
-        //If Crouch Is Released Resume Orginial Speed + Enable The Standing Collider + Enable Crouch Animation
-          if(isGrounded)
-        {
-            rb.velocity = Vector2.up * verticalPower; 
-            animator.SetBool("Jump",true);
-        }
-        else
-        {
-            if(coyoteJump)
-            {
-            rb.velocity = Vector2.up * verticalPower; 
-            animator.SetBool("Jump",true);
-            }
-        }
-    }
-
-
-    /* 
+/* 
     Method Name: move()
     Parameter: 
     float dir - Value associated with direction of player in regards to x-axis 
+    bool jumpFlag - Value assocaited with weather player is able to Jump or Not
     bool crouchFlag - Value assocaited with Weather Player is crouching or not
     Description: Manipulates Movement For Player
-    */     
-    void move(float dir,bool crouchFlag)
+*/     
+    void move(float dir, bool jumpFlag, bool crouchFlag)
     {
 
-        #region Crouch
+        #region Jump & Crouch
+
+
+
 
         //If we are crouching and disable crouching
         //check overhead for collision with ground items
@@ -347,11 +235,26 @@ public class PlayerMovement : MonoBehaviour
             }
     
         }
-        //Set StandingCollider to Negation Of CrouchFlag
-        standingCollider.enabled = !crouchFlag;
 
+        //If Crouch Is Pressed Disable The Standing Collider + Enable animate crouching + Reduce Speed
+        //If Crouch Is Released Resume Orginial Speed + Enable The Standing Collider + Enable Crouch Animation
+        if(isGrounded)
+        {
+            // Set StandingCollider to Negation Of CrouchFlag
+            standingCollider.enabled = !crouchFlag;
+    
+            //If The Player Is Grounded And Pressed `space` Jump
+            if(jumpFlag)
+            {
+                
+                jumpFlag = false;
+                //Add Jump Force
+                rb.AddForce(new Vector2(0f,verticalPower));
+            }
+        }
+         
         animator.SetBool("Crouch", crouchFlag);
-       
+     
         #endregion
      
 
@@ -363,7 +266,7 @@ public class PlayerMovement : MonoBehaviour
         float xVal = dir * speed * 100 * Time.fixedDeltaTime;
 
         // If player is running multiply with running modifier
-        if(isRunning && canRun)
+        if(isRunning)
         {
             xVal *= runSpeedModifer;
         }
@@ -384,16 +287,17 @@ public class PlayerMovement : MonoBehaviour
 
         //Facing direction for player sprite
         // If looking right and clicked right (flip to the left)
-        if(dir < 0 && isFacingRight)
+       if(dir < 0 && isFacingRight)
         {
             flip();
         }
        
-        // If looking left and clicked left (flip to the right)
+       // If looking left and clicked left (flip to the right)
         else if (dir > 0 && !isFacingRight)
         {
             flip();
         }
+
 
         // 0 idle, 4 walking , 8 running
         //Set the flot xVelocity accroding to the x value of the RigidBody2D Velocity
@@ -403,78 +307,16 @@ public class PlayerMovement : MonoBehaviour
     
     }
 
-    /* 
+  /* 
     Method Name: flip()
     Description: Changes the value of (boolean) "isFacingRight" from true/false 
     depending where player sprite if facing and rotates the player sprite to face 
     left or right
-    */  
+*/  
     private void flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0,180f,0f);
-    }
-
-
-    /* 
-    Method Name: HandleStamina()
-    Description: Handle Stamina 
-    */  
-    private void HandleStamina()
-    {
-        //Determine if player is moving while running 
-        if(isRunning && horizontalMovement != 0)
-        {
-            
-            //Determine if player is regenerating stamina
-            if(regeneratingStamina != null)
-            {
-                StopCoroutine(regeneratingStamina);
-                regeneratingStamina = null;
-            }
-            //Depletes current stamina 
-            currentStamina -= staminaUseMultiplier * Time.deltaTime;
-
-            if(currentStamina < 0)
-            {
-                currentStamina = 0;
-            }
-            if(currentStamina <= 0)
-            {
-                canRun = false;
-            }
-            
-        }
-        //Determine if player is not running and stamina has been used
-        if(!isRunning && currentStamina < maxStamina && regeneratingStamina == null)
-            {
-                //Start stamina regeneration process
-                regeneratingStamina = StartCoroutine(RegenerateStamina());
-            }
-    }
-
-
-    // Handle the stamina regeneration of player
-    private IEnumerator RegenerateStamina()
-    {
-        yield return new WaitForSeconds(timeBeforeStaminaRegenStarts);
-        WaitForSeconds timeToWait = new WaitForSeconds(staminaTimeIncrement);
-
-        while(currentStamina < maxStamina)
-        {
-            if(currentStamina > 0)
-            {
-                canRun = true;
-            }
-            currentStamina += staminaValueIncrement * Time.deltaTime;
-
-            if(currentStamina > maxStamina)
-            {
-                currentStamina = maxStamina;
-            }
-            yield return timeToWait;
-        }
-        regeneratingStamina = null;
     }
 }  
 
