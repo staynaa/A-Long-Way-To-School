4faa36661f409719dcb4119c3df795b4b26907f0
Stayna <staynaa.cs@gmail.com>
2023-05-08 18:58:22 -0400

    made some edits to nepal

---
+++ A_L_W_T_S/Assets/Scripts/notifyPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notifyPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject notification;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag=="notif")
+        notification.SetActive(true); //once player is back in bounds the text will disappear 
+    }
+}
