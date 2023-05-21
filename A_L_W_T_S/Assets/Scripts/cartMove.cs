a51ba0d9fd185ab5c1052af41829383bcb0a3daa
Stayna <staynaa.cs@gmail.com>
2023-05-21 01:35:34 -0400

    nepal level quiz
    test this out, if there is a loading issue let me know

--- A_L_W_T_S/Assets/Scripts/cartMove.cs
+++ A_L_W_T_S/Assets/Scripts/cartMove.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class cartMove : MonoBehaviour
 {
-    [SerializeField] float movement;
-    [SerializeField] Rigidbody2D rigid;
-    [SerializeField] int speed;
+    // [SerializeField] float movement;
+    // [SerializeField] Rigidbody2D rigid;
+    // [SerializeField] int speed;
+    [SerializeField] List<GameObject> btn=new List<GameObject>();
+    [SerializeField] GameObject imgObj;
     // Start is called before the first frame update
     void Start()
     {
-        if (rigid == null) rigid = GetComponent<Rigidbody2D>();
-        speed = 3;
+        // if (rigid == null) rigid = GetComponent<Rigidbody2D>();
+        // speed = 3;
     }
     // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxis("Horizontal");
+        // movement = Input.GetAxis("Horizontal");
+        transform.position += Time.deltaTime*Vector3.right * 3.0f;
     }
 
     //called potentially multiple times per frame
     //used for physics & movement
-    void FixedUpdate()
-    { 
-        rigid.velocity = new Vector2(movement * speed, rigid.velocity.y);
+    // void FixedUpdate()
+    // { 
+    //     rigid.velocity = new Vector2(movement * speed, rigid.velocity.y);
+    // }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "notif"){ 
+            Time.timeScale = 0;
+            imgObj.SetActive(false);
+            btn[0].SetActive(false);
+            btn[1].SetActive(false);
+            btn[2].SetActive(false);
+            btn[3].SetActive(false);
+        }
     }
 }
---
+++ A_L_W_T_S/Assets/Scripts/quiz.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class quiz : MonoBehaviour
+{
+    [SerializeField] Image flagImg;
+    [SerializeField] string correctText,opt1,opt2,opt3;
+    public List<Sprite> flags = new List<Sprite>();
+    private List<int> alreadyAsked=new List<int>();
+    [SerializeField] List<TextMeshProUGUI> choicesBtn=new List<TextMeshProUGUI>();
+    [SerializeField] TextMeshProUGUI scoreText;
+    public int score=0, quesNum=1,streakScore=1;
+    [SerializeField] GameObject curFlag;
+    void Start()
+    {
+        flagImg= curFlag.GetComponent<Image>(); //get image component
+        nextFlag();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void nextFlag(){
+        int i1=getRand(),i2=getRand(),i3=getRand(),i4=getRand();
+        while(((i1==i2)||(i2==i3)||(i3==i4)||(i4==i1)||(i3==i1)||(i4==i2))||(alreadyAsked.Contains(i1))){
+            i1=getRand(); //the correct answer
+            i2=getRand();
+            i3=getRand();
+            i4=getRand();
+        }
+        flagImg.sprite = flags[i1];
+        correctText=flagImg.sprite.name;
+        opt1=flags[i2].name;
+        opt2=flags[i3].name;
+        opt3=flags[i4].name;
+        alreadyAsked.Add(i1);
+        if(alreadyAsked.Count==flags.Count) alreadyAsked.Clear();
+
+        int x1=Random.Range(0,4),x2=Random.Range(0,4),x3=Random.Range(0,4),x4=Random.Range(0,4);
+        while((x1==x2)||(x2==x3)||(x3==x4)||(x3==x1)||(x4==x1)||(x4==x2)){
+            x1=Random.Range(0,4);
+            x2=Random.Range(0,4);
+            x3=Random.Range(0,4);
+            x4=Random.Range(0,4);
+        }
+        choicesBtn[x1].SetText(correctText);
+        choicesBtn[x1].name=correctText;
+
+        choicesBtn[x2].SetText(opt1);
+        choicesBtn[x2].name=opt1;
+
+        choicesBtn[x3].SetText(opt2);
+        choicesBtn[x3].name=opt2;
+
+        choicesBtn[x4].SetText(opt3);
+        choicesBtn[x4].name=opt3;
+    }
+    public int getRand(){
+        return Random.Range(0,flags.Count); //index for flags
+    }
+
+    public void checkAnswer(GameObject choice){
+        string ans=choice.name;
+        Debug.Log("ANSWER: "+correctText+" Your Answer: "+ans);
+        if(correctText.Equals(ans)){
+            score+=10*streakScore;
+            Debug.Log("CORRECT");
+            streakScore+=2; //gain streak score
+        }
+        else{
+            score-=20;
+            Debug.Log("INCORRECT");
+            streakScore=1; //lose streak score
+        }
+        scoreText.SetText("Score: "+score);
+        quesNum++;
+
+        nextFlag();
+    }
+}
