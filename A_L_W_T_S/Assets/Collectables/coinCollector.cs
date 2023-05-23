fe05b5e7116eb46fbb6cea359748464f2bb02d49
CeeDee <charles_dubreuil053100@yahoo.com>
2023-05-23 09:42:17 -0400

    ADD COIN SYSTEM

--- A_L_W_T_S/Assets/Collectables/coinCollector.cs
+++ A_L_W_T_S/Assets/Collectables/coinCollector.cs
@@ -1,34 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class coinCollector : MonoBehaviour
 {
-    private double totalCash = 0.0;
+    private double fareTotal = 0;
+    [SerializeField] private TMP_Text fareText;
+    
+
+
     private void OnTriggerEnter2D(Collider2D collision) 
     {
 
      if(collision.gameObject.CompareTag("Penny"))
      {
         Destroy(collision.gameObject);
-        totalCash += 0.1;
+        fareTotal += 0.1;
      }
      
      else if (collision.gameObject.CompareTag("Nickel"))
      {
         Destroy(collision.gameObject);
-        totalCash += 0.5;
+        fareTotal += 0.5;
      }
      
      else if (collision.gameObject.CompareTag("Quarter"))
      {
         Destroy(collision.gameObject);
-        totalCash += 0.25;
+        fareTotal += 0.25;
         
      }
 
-
-
-    
+      fareText.text = "Fare: $" + fareTotal;
      }
 }
